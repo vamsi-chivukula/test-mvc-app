29f43ff117535c588670bd27c666b3daae88e4cd
vamsichivukula <vamsichivu@gmail.com>
2022-08-22 22:39:28 -0400

    initial commit

---
+++ test-mvc-app/Models/PersonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace test_mvc_app.Models
+{
+    public class PersonModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; } = 0;
+        public bool IsAlive { get; set; } = true;
+    }
+}
